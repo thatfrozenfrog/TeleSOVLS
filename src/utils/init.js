cbb8a5e5b38d129facdce983d09121e4daf116c5
thatfrozenfrog <101154752+thatfrozenfrog@users.noreply.github.com>
2025-11-16 16:11:16 +0700

    Create a new init file for cleaner code

---
+++ src/utils/init.js
@@ -0,0 +1,22 @@
+import { changeTheme } from "../utils/theme";
+import { initAuto2048Tool } from "./auto/auto2048";
+import { initTypespeedTool } from "./auto/autotypespeed";
+import { applyTheme } from "../utils/theme";
+import { initThemeTool } from "../utils/theme";
+import { initporthackui } from "./auto/autoporthack";
+export function initUI() {
+  initThemeTool();
+  initAuto2048Tool();
+  initTypespeedTool();
+  initporthackui();
+  try {
+    const savedTheme = localStorage.getItem("th-selected-theme");
+    if (savedTheme && typeof savedTheme === "string") {
+      changeTheme(savedTheme);
+    } else {
+      changeTheme("Casual");
+    }
+  } catch (_) {
+    changeTheme("Casual");
+  }
+}
